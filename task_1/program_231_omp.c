412951f1e9828edc9e18534eb5d7de182bc5aa51
Vladimir Lazarev <Lazarewvladimir18@gmail.com>
2021-10-06 00:29:42 +0300

    task 1 done

---
+++ task_1/program_231_omp.c
@@ -0,0 +1,21 @@
+for (i = 2; i <= n+1; ++i) {
+	C[i] = C[i + 1] + D[i];
+}
+
+for (i = 2; i <= n+1; ++i) {
+	for(j = 2; j <= m+1; ++j) {
+		B[i][j] = B[i + 1][j - 2] + C[i];
+	}
+}
+
+for (i = 2; i <= n+1; ++i) {
+	A[i][1][1] = C[n + 1];
+
+	#pragma omp parallel for
+	for(j = 2; j <= m+1; ++j) {
+		#pragma omp parallel for
+		for(k = 1; k <= n; ++k) {
+			A[i][j][k] = A[i - 1][j][1] + A[i][j][k];
+		}
+	}
+}
